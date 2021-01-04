3165365aee0fb2e870e91eb96a90990851141a4e
Nguyễn Thành Nhân <nnhan0719@gmail.com>
2021-01-04 23:08:09 +0700

    Initial Commit

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Nhan Nguyen on 1/4/21.
+//  Copyright © 2021 Nguyen Nhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
